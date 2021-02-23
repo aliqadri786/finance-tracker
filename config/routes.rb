7988e26c50c6c2eea8f920f68e08a238f69885fa
Ali Akbar <aliqadri100@gmail.com>
2021-02-23 14:32:46 +0500

    final

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   root 'welcome#index'
 
   get 'my_portfolio', to:'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
 
- 
+  get 'my_friends', to: 'users#my_friends'
   resources :user_stocks, only: [:create, :destroy]
+
+  resources :users, only: [:show]
+  resources :friendships
+
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
 end
